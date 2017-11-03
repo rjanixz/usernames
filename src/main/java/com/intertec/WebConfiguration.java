715b564954afbe23e6fd6ee19e075518194cda50
Rainman Sián <ray.isistemas@gmail.com>
2017-11-02 21:52:15 -0600

    Added configuration to enable H2 console

--- src/main/java/com/intertec/WebConfiguration.java
+++ src/main/java/com/intertec/WebConfiguration.java
@@ -1,4 +1,21 @@
 package com.intertec;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Configuration needed for the H2 database console
+ */
+@Configuration
 public class WebConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
 }
